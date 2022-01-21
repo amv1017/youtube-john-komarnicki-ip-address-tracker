f0bcc9fc2cb143732982c3c75144ca2dd7f62806
amv1017 <Vinokurov@bbs-c.ru>
2022-01-21 19:42:53 +0300

    store & reducers

---
+++ react-redux-crash-course/src/components/Banner.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Banner() {
+  return(
+    <h1>{'React & Redux App'}</h1>
+  )
+}
---
+++ react-redux-toolkit-crash-course/src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div>
+      <h1>React & Redux Toolkit App</h1>
+    </div>
+  )
+}
