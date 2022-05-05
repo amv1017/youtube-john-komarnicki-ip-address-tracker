0972dd4f53a1b0241f9c9f9402d1eab4c01a6a33
amv1017 <Vinokurov@bbs-c.ru>
2022-05-05 17:34:33 +0300

    hook use cases

--- react-hooks-crash-course/src/components/ComponentWithUseReducer.js
+++ react-hooks-crash-course/src/components/ComponentWithUseReducer.js
@@ -1,7 +1,47 @@
+import { useReducer } from 'react'
+
+const ACTIONS = {
+  DEPOSIT: "DEPOSIT",
+  WITHDRAW: "WITHDRAW"
+}
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case ACTIONS.DEPOSIT: {
+      return state + action.payload;
+    }
+    case ACTIONS.WITHDRAW: {
+      return state - action.payload;
+    }
+    default: {
+      return state
+    }
+  }
+}
+
 export default function ComponentWithUseReducer() {
+
+  const deposit = (amount) => {
+    dispatch({
+      type: ACTIONS.DEPOSIT,
+      payload: amount
+    })
+  }
+
+  const withdraw = (amount) => {
+    dispatch({
+      type: ACTIONS.WITHDRAW,
+      payload: amount
+    })
+  }
+
+  const [amount, dispatch] = useReducer(reducer, 500)
+
   return (
     <div>
-      Hello
+      <h1>{amount}</h1>
+      <button onClick={() => deposit(500)}>Deposit</button>
+      <button onClick={() => withdraw(500)}>Withdraw</button>
     </div>
   )
 }
