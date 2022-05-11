63b8386b5f115e329285481d55646f5978e7724f
amv1017 <Vinokurov@bbs-c.ru>
2022-05-11 17:43:39 +0300

    icons set

--- src/router/index.js
+++ src/router/index.js
@@ -5,13 +5,21 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: HomeView,
+    meta: {
+      title: 'IP Address Tracker'
+    }
   }
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  document.title = `${to.meta.title}`
+  next()
 })
 
 export default router
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,28 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="flex flex-col h-screen max-h-screen">
+    <!-- search / results -->
+    <div class="flex justify-center relative bg-hero-pattern px-4 pt-8 pb-32">
+      <!-- search input -->
+      <div class="w-full max-w-screen-sm">
+        <h1 class="text-white text-center text-3xl pb-4">IP Address Tracker</h1>
+        <div class="flex">
+          <input class="flex-1 py-3 px-2 rounded-tl-md rounded-bl-md focus:outline-none"
+                  type="text"
+                  placeholder="Search for any IP address or leave empty to get your IP info"
+          >
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
   }
 }
 </script>
